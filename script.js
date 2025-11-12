275fa70cf6a4f0a599ecf81e3850a7faf30fcfc2
raulbutuza334-jpg <raulbutuza334@gmail.com>
2025-11-12 19:19:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// cache the DOM
+var button = document.getElementById("button")
+
+var nume = document.getElementById("nume")
+
+var prenume = document.getElementById("prenume")
+
+var asteptari = document.getElementById("asteptari")
+
+var img = document.querySelector("img")
+
+var an = document.getEelementById("anNastere")
+
+const d = new Date()
+Var year = d.getFullYear()
+
+//Add event listener to button
+button.addEventlistener("click",altaViata)
+
+//Define function
+function altaViata() {
+         nume.innerHTML = "Softaware Engineer"
+prenume.innerHTML = 
+
+
+         asteptari.innerHTML = "<ul><li>Libertate
+financiara</li><li>Educatie superioara</li><li>"
+
+        img.src = "images/floare.jpg
+        img.style.opacity = "70%"
+        img.style.height = "200px"
+        img.styles.width = "300px"
+        img.style.border = "3px solid black"
+ 
+        body.style.backgroundSize = "cover"
+        body.style.background.Repeat = "no-repeat"
+        body.style.background.Position = "Center"
+        body.style.background.Attachment = "fixed"
+
+        body.style.color = "yellow"
+        body.style.fontFamily = "Montserrat"
+
+
+
+     
+     
+}
